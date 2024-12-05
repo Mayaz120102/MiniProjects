da2cb546a990e1f79316c46b491994608be5e69a
Mayaz120102 <abrarmayaz2002@gmail.com>
2024-12-05 21:58:31 +0600

    app.js adding using fetch api

--- Recip_app/app.js
+++ Recip_app/app.js
@@ -1,3 +1,19 @@
-const searchBox = document.querySelectorAll(".searchBox");
-const searchBtn = document.querySelectorAll(".searchBtn");
-const recipeContainer = document.querySelectorAll(".recipe-container");
+const searchBox = document.querySelector(".searchBox");
+
+const searchBtn = document.querySelector(".searchBtn");
+
+const recipeContainer = document.querySelector(".recipe-container");
+
+const recipes = () => {
+  const data = fetch(`www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`);
+};
+
+searchBox.addEventListener("click", () => {
+  console.log("clicked");
+});
+
+searchBtn.addEventListener("click", () => {
+  const searchInput = searchBox.ariaValueMax.trim();
+  recipes();
+  console.log("nice worked hehe");
+});
