3048676ed2a2e41ccd0f116ca2ab5af2305179f1
Mayaz120102 <abrarmayaz2002@gmail.com>
2024-12-06 00:18:55 +0600

    implementing modals

--- Recip_app/app.js
+++ Recip_app/app.js
@@ -1,19 +1,50 @@
 const searchBox = document.querySelector(".searchBox");
-
 const searchBtn = document.querySelector(".searchBtn");
-
 const recipeContainer = document.querySelector(".recipe-container");
+const recipeDetailsContent = document.querySelector(".recipe-details-content");
+const closeBtn = document.querySelector(".closeBtn");
 
-const recipes = () => {
-  const data = fetch(`www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`);
+const recipes = async (query) => {
+  recipeContainer.innerHTML = "seaching for recipe";
+  const data = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
+  );
+  const response = await data.json();
+
+  recipeContainer.innerHTML = "";
+  response.meals.forEach((meal) => {
+    const recipeDiv = document.createElement("div");
+    recipeDiv.classList.add("recipe");
+    recipeDiv.innerHTML = `
+     <img src ="${meal.strMealThumb}">
+     <h3>${meal.strMeal}</h3>
+     <p>${meal.strArea}</p>
+     <p>${meal.strCategory}</p>
+    `;
+    const recipbtn = document.createElement("button");
+    recipbtn.textContent = "View Recipe";
+    recipeDiv.appendChild(recipbtn);
+    recipbtn.addEventListener("click", () => {
+      openRecipePopup(meal);
+    });
+
+    recipeContainer.appendChild(recipeDiv);
+
+    // console.log(meal);
+  });
+  //   console.log(response);
 };
 
-searchBox.addEventListener("click", () => {
-  console.log("clicked");
-});
+const openRecipePopup = (meal) => {
+  recipeDetailsContent.innerHTML = `
+   <h2>${meal.strMeal}</h2>
+   `;
+  recipeDetailsContent.parentElement.style.display = "block";
+};
 
 searchBtn.addEventListener("click", () => {
-  const searchInput = searchBox.ariaValueMax.trim();
-  recipes();
-  console.log("nice worked hehe");
+  const searchInput = searchBox.value.trim();
+  recipes(searchInput);
+
+  //   console.log("nice worked hehe");
 });
